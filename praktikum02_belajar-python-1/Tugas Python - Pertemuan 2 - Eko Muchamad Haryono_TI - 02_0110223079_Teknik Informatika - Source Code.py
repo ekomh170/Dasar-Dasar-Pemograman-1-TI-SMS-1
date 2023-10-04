676c9df8b221c4062e79f50c430bf6cf7e7f25a2
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2023-10-05 00:25:17 +0700

    2.0 - Tugas DDP Day/Praktikum 2 - Tentang Menggunakan Python 1

---
+++ praktikum02_belajar-python-1/Tugas Python - Pertemuan 2 - Eko Muchamad Haryono_TI - 02_0110223079_Teknik Informatika - Source Code.py
@@ -0,0 +1,28 @@
+print("");
+# Soal : 1. buat program dengan bahasa python, untuk mencetak nama, nim, kelas dan alamat 
+# Jawaban Nomer 1
+print("1. Nama : Eko Muchamad Haryono, NIM : 0110223079, Kelas : TI-02, Alamat : Citeureup");
+
+# 2. Soal : buat seperti no 1 tapi 1 teman kalian
+# Jawaban Nomer 2
+print("2. Nama : Yossy Indra Kusuma, NIM : 0110223073, Kelas : TI-02, Alamat : Bojong Gede");
+
+# 3. Soal : buat program dengan bahasa python, untuk menjumlahkan 2 buah bilangan
+# Jawaban Nomer 3
+
+print(350+100);
+
+# 4. Soal : buat program dengan bahasa python, untuk mengurangi 2 buah bilangan 
+# Jawaban Nomer 4
+print(350-100);
+
+# 5. Soal : buat program dengan bahasa python, untuk mengalikan 2 buah bilangan
+# Jawaban Nomer 5
+print(50*2);
+
+# 6. buat program dengan bahasa python, untuk membagi 2 buah bilangan. 
+# Jawaban Nomer 6
+print(50/2);
+
+print("");
+print("");
