6df63551d9ae69291eb100ffa3a91163bae685dd
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2023-10-25 08:33:44 +0700

    memperbarui folder praktikum 4 dan menambahkan praktikum 5

---
+++ praktikum05_operator-if-2/hello.py
@@ -0,0 +1,33 @@
+# If Condition
+a = 10
+b = 15
+if b> a:
+    print("b lebih besar dari a")
+    
+# Elif Condition
+a = 10
+b = 10
+if b > a:
+    print("b lebih besar dari a")
+elif a == b:
+    print("a sama dengan b")
+    
+# Else
+a = 100
+b = 10
+if b> a:
+    print("b lebih besar dari a")
+elif a == b:
+    print("a sama dengan b")
+else:
+    print("a lebih besar dari b")
+    
+# Short Hand If and if else
+
+if a > b: print("a is greater than b")
+
+# Atau
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
