4d0693ef59ddd092064728abb6c8521204d185c4
Eko Muchamad Haryono <ekomh13@gmail.com>
2023-11-08 08:07:48 +0700

    menambahkan tugas python 6

--- praktikum06_perulangan/tugas/praktikum_6.py
+++ praktikum06_perulangan/tugas/praktikum_6.py
@@ -3,11 +3,10 @@
 index = 1
 numbers = [ 951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 547, 544,  615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 984, 592, 236, 105, 942, 941,  386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,  399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440, 380, 126, 721, 328, 753, 470, 743, 527 ]
 
-# Menggunakan loop while untuk mencetak bilangan ganjil sampai bertemu dengan 553
 while index < len(numbers):
-    if numbers[index] % 2 != 0:  # Cek apakah bilangan ganjil
+    if numbers[index] % 2 != 0:  
         print(numbers[index])
-        if numbers[index] == 553:  # Hentikan perulangan jika sudah mencapai 553
+        if numbers[index] == 553: 
             break
     index += 1
 
